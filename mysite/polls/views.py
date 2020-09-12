53824272da1626f7270e014ce88a34939e9122c7
AuttaKrit Wongsarawit <auttakrit.w@ku.th>
2020-09-12 14:53:23 +0700

    Task6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,7 +14,9 @@ class IndexView(generic.ListView):
         """
         Return the last five published questions(not including those set to be published in the future)
         """
-        return Question.objects.filter(pub_date_lte=timezone.now()).order_by('pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -24,7 +26,7 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date_lie=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
