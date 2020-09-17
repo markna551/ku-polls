d12b79d92936d0bc94eb695926b12c6cc71b6579
AuttaKrit Wongsarawit <auttakrit.w@ku.th>
2020-09-17 22:02:54 +0700

    update test

--- polls/tests.py
+++ polls/tests.py
@@ -122,31 +122,49 @@ class QuestionDetailViewTests(TestCase):
 class QuestionCanVoteTest(TestCase):
 
     def test_can_vote_question(self):
+        """
+        can_vote() returns True for questions whose pub_date in the past
+        and end_date in the future.
+        """
         pubtime = timezone.now() - datetime.timedelta(days=1, seconds=1)
         endtime = timezone.now() + datetime.timedelta(days=1, seconds=1)
         canvote_question = Question(pub_date=pubtime ,end_date=endtime)
         self.assertIs(canvote_question.can_vote(), True)
 
     def test_can_vote_future_question(self):
+        """
+        can_vote() returns False for questions whose pub_date in the future
+        and end_date in the future.
+        """
         pubtime = timezone.now() + datetime.timedelta(days=3, seconds=1)
         endtime = timezone.now() + datetime.timedelta(days=1, seconds=1)
         canvote_question = Question(pub_date=pubtime ,end_date=endtime)
         self.assertIs(canvote_question.can_vote(), False)
 
     def test_can_vote_ended_question(self):
+        """
+        can_vote() returns False for questions whose pub_date in the past
+        and end_date in the past.
+        """
         pubtime = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        endtime = timezone.now() - datetime.timedelta(seconds=1)
+        endtime = timezone.now() - datetime.timedelta(days=2, seconds=1)
         canvote_question = Question(pub_date=pubtime, end_date=endtime)
         self.assertIs(canvote_question.can_vote(), False)
 
 class QuestionIsPublishedTest(TestCase):
 
     def test_is_published_question(self):
+        """
+        is_published() returns True for questions whose pub_date in the past.
+        """
         pubtime = timezone.now() - datetime.timedelta(days=1, seconds=1)
         published_question = Question(pub_date=pubtime)
         self.assertIs(published_question.is_published(), True)
 
     def test_is_published_future_question(self):
+        """
+        is_published() returns True for questions whose pub_date in the future.
+        """
         pubtime = timezone.now() + datetime.timedelta(days=1, seconds=1)
         published_question = Question(pub_date=pubtime)
         self.assertIs(published_question.is_published(), False)
